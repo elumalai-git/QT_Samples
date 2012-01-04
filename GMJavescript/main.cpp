ad5ca782d46cdb1903268496a5d775c433bad2ee
Elumalai <linux.elumalai@gmail.com>
2012-01-04 15:56:34 +0530

    GMJS

--- GMJavescript/main.cpp
+++ GMJavescript/main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     JavaScript w;
-//    w.load(QUrl("file:///home/elumalai/Downloads/qtwebkittrainingexampleattachedeom/test.html"));
     w.load(QUrl("file:://home/elumalai/Documents/test.html"));
     w.show();
 
